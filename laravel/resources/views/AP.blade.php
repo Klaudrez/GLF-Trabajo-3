563dfe9012d319aa7d3f57fd66280fa5afd1bbad
Klaudrez <andres.paradac@utem.cl>
2020-11-27 20:58:08 -0300

    Estado inicial y finales en alertas
    se agregaron los estados iniciales y finales a las alertas

--- laravel/resources/views/AP.blade.php
+++ laravel/resources/views/AP.blade.php
@@ -209,7 +209,7 @@ function ValidarEntrada1()
 
               graph()
 
-              alert(strestados(ConjuntoQ1) +strgama(Gama1))
+              alert("Estado inicial : \n"+E_inicial1 + "\n" + strfinales(E_Finales1) + strestados(ConjuntoQ1) +strgama(Gama1))
             }
             else{
               alert("Lo datos ingresados no validos, deben estar contenidos en el alfabeto o en los estados")
@@ -304,7 +304,7 @@ function ValidarEntrada2()
               mostrardatos(ConjuntoQ2,Alfabeto2,AlfabetoP2,Gama2,E_inicial2,E_Finales2,"A2")
 
               graph()
-              alert(strestados(ConjuntoQ2) +strgama(Gama2))
+              alert("Estado inicial : \n"+E_inicial2 + "\n" + strfinales(E_Finales2) + strestados(ConjuntoQ2) +strgama(Gama2))
             }
             else{
               alert("Lo datos ingresados no validos, deben estar contenidos en el alfabeto o en los estados")
@@ -429,7 +429,7 @@ function Union()
 
       mostrardatos(ConjuntoCombi,Alfabeto1,AlfabetoP1,GamaCombi,E_inicialCombi,E_FinalesCombi,"Au")
       graph()
-      alert(strestados(ConjuntoCombi) +strgama(GamaCombi))
+      alert("Estado inicial : \n"+E_inicialCombi + "\n" + strfinales(E_FinalesCombi) + strestados(ConjuntoCombi) +strgama(GamaCombi))
     }
     else
       alert("Los automatas no tienen coincidencias en sus alfabetos")
@@ -506,7 +506,7 @@ function Concatenar()
             mostrardatos(ConjuntoCombi,AlfabetoCombi,AlfabetoPCombi,GamaCombi,E_inicialCombi,E_FinalesCombi,"Ac")
 
             graph()
-            alert(strestados(ConjuntoCombi) +strgama(GamaCombi))
+            alert("Estado inicial : \n"+E_inicialCombi + "\n" + strfinales(E_FinalesCombi) + strestados(ConjuntoCombi) +strgama(GamaCombi))
           }
           else
           {
@@ -546,7 +546,7 @@ function Concatenar()
             mostrardatos(ConjuntoCombi,AlfabetoCombi,AlfabetoPCombi,GamaCombi,E_inicialCombi,E_FinalesCombi,"Ac")
 
             graph()
-            alert(strestados(ConjuntoCombi) +strgama(GamaCombi))
+            alert("Estado inicial : \n"+E_inicialCombi + "\n" + strfinales(E_FinalesCombi) + strestados(ConjuntoCombi) +strgama(GamaCombi))
           }
         }
         else
@@ -954,15 +954,31 @@ function existealfa(string,arreglo)
 
 function strgama(gama)
 {
-    var str="y las transiciones son: \n"
+    var str="Transicion(es) : \n"
   for(let i=0;i<gama.length;i++)
       str=str+gama[i][0]+" - "+gama[i][1]+"/"+gama[i][2]+"/"+gama[i][3]+" - "+gama[i][4]+"\n"
      return str
 }
 
+function strfinales(gama)
+{
+    var str="Estado(s) final(es) : \n"
+  for(let i=0;i<gama.length;i++){
+    if(i!=gama.length-1){
+
+      str=str+gama[i]+" - "
+    }
+    else{
+      str=str+gama[i]+"\n"
+    }
+  }
+      
+     return str
+}
+
 function strestados(estados)
 {
-  var str="Los estados son: \n"
+  var str="Estado(s) : \n"
   for(let i=0;i<estados.length;i++){
     if(i!=estados.length - 1){
        str=str+estados[i]+" - "
